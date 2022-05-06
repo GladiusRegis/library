a8fc71347d6458aefd905c6b7bc25f6df4557ae6
Gladius Regis <gladiusregis@gmail.com>
2022-05-06 20:44:04 +0200

    clear code

--- library/main.py
+++ library/main.py
@@ -1,19 +1,29 @@
 import sqlite3
 
+
+def get_books(c):
+    return c.execute('SELECT book_id, title, author FROM books')
+
+
+def save_book(conn, t, a):
+    c = conn.cursor()
+    c.execute('INSERT INTO books(title, author) VALUES(?, ?)', (t, a))
+    conn.commit()
+
+
 action = input('What do you want to do? [p]rint, [a]dd: ')
 if action == 'p':
     with sqlite3.connect('library.db') as connection:
         cursor = connection.cursor()
-        for book in cursor.execute('SELECT book_id, title, author FROM books'):
+        for book in get_books(cursor):
             # print(book)
             book_id, title, author = book
             print(f'ID: {book_id}, title: {title}, author: {author}')
 elif action == 'a':
     with sqlite3.connect('library.db') as connection:
-        cursor = connection.cursor()
         title = input('Title: ')
         author = input('Author: ')
-        cursor.execute('INSERT INTO books(title, author) VALUES(?, ?)', (title, author))
-        connection.commit()
+        save_book(connection, title, author)
+
 else:
     print('Select [p] or [a]')
