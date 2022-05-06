b75a62202190300ae0e7f625e532082ec1d6cb71
Gladius Regis <gladiusregis@gmail.com>
2022-05-06 22:27:56 +0200

    added test_meetings

---
+++ meetings/main.py
@@ -0,0 +1,10 @@
+from datetime import datetime, timedelta
+from meetings.Meeting import Meeting
+from meetings.Calendar import Calendar
+
+if __name__ == '__main__':
+    opction =
+
+    print('Calendar 2022')
+
+
